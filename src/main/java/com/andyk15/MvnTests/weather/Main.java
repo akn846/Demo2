0bc766d0e0c15a2d1288332b88b612dc8301e331
Andy <akn846@tempest.jak>
2016-01-17 18:16:30 +0000

    Maven example 1

---
+++ src/main/java/com/andyk15/MvnTests/weather/Main.java
@@ -0,0 +1,40 @@
+package com.andyk15.MvnTests.weather; 
+
+import java.io.InputStream;
+import org.apache.log4j.PropertyConfigurator;
+
+public class Main
+{
+
+	public static void main(String[] args) throws Exception
+	{
+
+		// Setup log4J cofig
+		PropertyConfigurator.configure(Main.class.getClassLoader().getResource("log4j.properties"));
+		// Defime defai;t ZIP code
+		String zipcode="60202";
+		try
+		{
+			zipcode=args[0];
+		} catch (Exception e){ 	}
+
+		new Main(zipcode).start();
+	}
+
+	private String zip;
+
+	public Main(String zip)
+	{
+		this.zip=zip;
+	}
+
+	public void start() throws Exception
+	{
+
+		InputStream dataIn = new YahooRetriever().retrieve(zip);
+
+		Weather weather = new YahooParser().parse(dataIn);
+
+		System.out.println(new WeatherFormatter().format(weather));
+	}
+}
